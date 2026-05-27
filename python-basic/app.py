dd72ff0c9886bf77c053aab2b4d4df54a585fb15
waltergauto <61380032+waltergauto@users.noreply.github.com>
2026-05-27 15:51:44 -0300

    Update app.py

--- python-basic/app.py
+++ python-basic/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     entorno = os.getenv('APP_ENV', 'Desarrollo')
-    return f"Aplicación do it v2 en el entorno de: {entorno}\n"
+    return f"Aplicación do it v3 en el entorno de: {entorno}\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
